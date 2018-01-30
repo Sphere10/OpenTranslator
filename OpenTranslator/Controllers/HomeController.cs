cb85438f1659898a43b4a86b30120c3dfdc3a25a
marualderete <maru.ferri@gmail.com>
2018-01-30 14:40:51 -0200

    Updating comment in Home Controller for CR

--- OpenTranslator/Controllers/HomeController.cs
+++ OpenTranslator/Controllers/HomeController.cs
@@ -3,6 +3,7 @@
 namespace OpenTranslator.Controllers
 {
 //This is a comment to test CR on github
+//This is another comment to merge in OT Fork!
     public class HomeController : Controller
     {
         public ActionResult Index()
