abc4b14996919edd99582180a37f8c4a538eb3db
DESKTOP-DPDP0CH\Maru <maria@argeniss.com>
2018-02-02 17:40:25 -0200

    validations on Text and Translation are now in Repository class in order to be reused everywhere

--- OpenTranslator/Repostitory/ITranslation.cs
+++ OpenTranslator/Repostitory/ITranslation.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Generic;
 
 using OpenTranslator.Data;
+using OpenTranslator.Models.Input;
 
 namespace OpenTranslator.Repostitory
 {
     public interface ITranslation : IBaseRepository<Translation>
     {
-		void InsertTextTranslation(Text text, Translation translation,TranslationLog translationlog); // C
-		IEnumerable<Text> GetText(); // R
-		Translation GetTranslationID(string Id); // R
-		List<Translation> GetTranslationLogByCode(string TextId,string LanguageCode);
-		List<Translation> GetTranslationByTextID(string TextId); 
-	}
+        void InsertTextTranslation(Text text, Translation translation, TranslationLog translationlog); // C
+        IEnumerable<Text> GetText(); // R
+        Translation GetTranslationID(string Id); // R
+        List<Translation> GetTranslationLogByCode(string TextId, string LanguageCode);
+        List<Translation> GetTranslationByTextID(string TextId); 
+        bool IsTranslateAlreadyStoraged(AdminInput input, string translatedText);
+        bool IsTextIdAlreadyStoraged(AdminInput input);
+    }
 }
